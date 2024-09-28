44564dbc33768ed202d4817ea4c5465721060266
yuvacharan13 <57321298+yuvacharan13@users.noreply.github.com>
2024-09-28 18:15:04 +0530

    Added privacy page

---
+++ app/privacy/page.js
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Privacy() {
+  return (
+    <div className={styles.page}>
+      Information Collection We collect personal data (such as name, email
+      address, and phone number) only when you provide it voluntarily through
+      forms or interactions on our app/website. Use of Information We use the
+      collected information to improve user experience, provide customer
+      support, and notify you of updates. Your information will not be shared
+      with third-party entities without your consent. Data Protection We
+      implement strict security measures to protect your personal data from
+      unauthorized access, disclosure, or alteration. This includes encryption
+      and secure storage practices. Cookies and Tracking Technologies Our
+      website may use cookies or similar tracking technologies to analyze user
+      behavior, manage the website, and track movements around the site. You can
+      control cookie preferences in your browser settings. Third-Party Services
+      Our app/website may integrate with third-party services (e.g., payment
+      gateways, analytics tools) that collect data in line with their privacy
+      policies. We are not responsible for the data practices of these third
+      parties. User Rights You have the right to request access, correction, or
+      deletion of your personal data at any time. Contact us for any queries
+      regarding your personal information. Childrens Privacy Our services are
+      not intended for children under the age of 13. We do not knowingly collect
+      personal information from children. Changes to this Policy We may update
+      this privacy policy periodically to reflect changes in our practices or
+      for legal, regulatory, or operational reasons. Any significant changes
+      will be communicated via email or a notice on the website. Contact
+      Information If you have any questions or concerns about our privacy
+      policy, please contact us at [Your Contact Information].
+    </div>
+  );
+}
